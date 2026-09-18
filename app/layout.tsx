1448fcccd09a03d3b73ac39b03051d962399b53b
Victor Facundo <126690596+VictorFOliveira@users.noreply.github.com>
2026-09-17 23:31:06 -0300

    style(phase6): load diary styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./phase2.css";
 import "./phase3.css";
 import "./phase4.css";
 import "./phase5.css";
+import "./phase6.css";
 
 export const metadata: Metadata = {
   title: "Minha Escola SaaS",
